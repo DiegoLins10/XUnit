e2a14a20adb779879144dee0ac641db79b9a7616
diegolins10 <dede140126>
2024-06-17 18:31:34 -0300

    Add project files.

---
+++ XUnit/Calculadora.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace XUnitCalculator
+{
+    public static class Calculadora
+    {
+        public static int Somar(int n1, int n2)
+        {
+            return n1 + n2;
+        }
+        public static int Subtrair(int n1, int n2)
+        {
+            return n1 - n2;
+        }
+        public static int Multiplicar(int n1, int n2)
+        {
+            return n1 *  n2;
+        }
+        public static int Dividr(int n1, int n2)
+        {
+            return n1 / n2;
+        }
+    }
+}
